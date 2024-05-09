e3002095f05220afa8cba8049ba82d195fa514cf
bhabha2 <139860951+bhabha2@users.noreply.github.com>
2024-05-09 19:00:27 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
-  baseURL: 'https://ven01957.service-now.com/', 
+  baseURL: 'https://yourinstance.service-now.com/', 
   sysparmLimit: 10,
   fieldsToBeExtracted: 'number,assigned_to,short_description,sys_id,description, severity,priority,active,sys_created_by,sys_created_on,sys_updated_by,sys_updated_on,work_notes',
 
